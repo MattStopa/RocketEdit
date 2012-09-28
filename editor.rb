35d7e0c4de0e4d298972c4c5987a5d313c1cd168
Matthew Stopa <matthew.p.stopa@gmail.com>
2012-09-27 23:56:49 -0400

    Started major refactor of code. Getting cleaner

--- editor.rb
+++ editor.rb
@@ -8,11 +8,15 @@ class Buffer
 end
 
 class Editor
-  attr_accessor :offset, :buffer
+  attr_accessor :offset, :buffer, :previous_x, :previous_y
 
   NEW_LINE = 10
   LEFT = Curses::Key::LEFT
   RIGHT = Curses::Key::RIGHT
+  UP = Curses::Key::UP
+  DOWN = Curses::Key::DOWN
+  BACKSPACE = 127
+  DC = Curses::Key::DC
 
   def initialize
     @offset = 0
@@ -30,66 +34,21 @@ class Editor
     arr.each_with_index do |element, index|
       win.addstr(spaces[index])
       if element[0] == ':'
-        win.attron(Curses.color_pair(3))
+        main_window.color_on(3)
       elsif element[0] == '"' || element[0] == "'"
-        win.attron(Curses.color_pair(4))
+        main_window.color_on(4)
       elsif special_words.include?(element)
-        win.attron(Curses.color_pair(2))
+        main_window.color_on(2)
       end
       win.addstr("#{element} ")
       win.attron(Curses.color_pair(1))
     end
   end
 
-  def handle_return_key(x, win, buffer)
-    if x == 10
-      buffer.insert(win.cury, '')
-      old_pos = [win.cury, win.curx]
-      redraw_screen(win, buffer)
-      win.setpos(old_pos.first + 1, old_pos.last)
-    end
-  end
 
-  def move(win, buffer, direction, offset=0)
-    current_line = buffer[win.cury - 1]
-    previous_line = buffer[win.cury - 2]
-    next_line = buffer[win.cury]
-    case direction
-    when :left
-      win.curx == 0 || win.curx == 1 ? win.setpos(win.cury, 0) : win.setpos(win.cury, win.curx - 1)
-    when :right
-      win.setpos(win.cury, win.curx + 1)
-      align_on_right(win, buffer, current_line)
-    when :up
-      redraw_screen(win, buffer, 0, offset)
-      win.cury == 0 || win.cury == 1 ? win.setpos(1, win.curx) : win.setpos(win.cury - 1, win.curx)
-      align_on_right(win, buffer, previous_line)
-    when :down
-      redraw_screen(win, buffer, 0, offset)
-      win.cury >= buffer.size ? win.setpos(buffer.size, win.curx) : win.setpos(win.cury + 1, win.curx)
-      align_on_right(win, buffer, next_line) if !(win.cury == buffer.size)
-    end
-  end
-
-  def align_on_right(win, buffer, current_line)
-     current_line.size < win.curx + 1 ? win.setpos(win.cury, current_line.size - 1) : win.setpos(win.cury, win.curx)
-  end
-
-  def redraw_screen(win, buffer, start=0, buffer_offset=0)
-    index = 0
-    old_pos = [win.cury, win.curx]
-    (buffer_offset..buffer_offset+52).each do |num|
-      win.setpos(index + 1, 0)
-      win.clrtoeol
-      writeln(win, num, buffer[num], buffer)
-      index += 1
-    end
-    win.setpos(old_pos.first, old_pos.last)
-
-  end
 
   def main_window
-    @window ||= Curses::Window.new( 0, 0, 0, 0)
+    @main_window ||= Window.new
   end
 
   def initialize_app
@@ -101,15 +60,14 @@ class Editor
     Curses.init_pair(4, Curses::COLOR_CYAN, Curses::COLOR_BLACK)
     Curses.noecho()
 
-    main_window.attron(Curses.color_pair(1));
-    main_window.keypad(true)
-    self.buffer = []
+    main_window.color_on(1)
+    main_window.buffer = []
   end
 
   def load_file
     file = File.open(ARGV.first, 'r')
     while (l = file.gets)
-      self.buffer << l
+      main_window.buffer << l
     end
   end
 
@@ -117,48 +75,130 @@ class Editor
     initialize_app
     load_file
 
-    redraw_screen(main_window, buffer)
-    main_window.refresh
+    main_window.redraw_screen
 
-    lines = buffer.count
 
     while 1
-      x = main_window.getch
-      case x
-      when NEW_LINE #new line
-        handle_return_key(x, main_window, buffer)
+      key_input =
+      case key_input
+      when NEW_LINE
+        main_window.handle_return_key(key_input, buffer)
       when LEFT
-        move(main_window, buffer, :left)
-      when Curses::Key::RIGHT
-        move(main_window, buffer, :right)
-      when Curses::Key::UP
-        self.offset = offset - 1 if main_window.cury == 1 && offset > 0
-        move(main_window, buffer, :up, offset)
-      when Curses::Key::DOWN
-        self.offset = offset + 1 if main_window.cury > 50
-        move(main_window, buffer, :down, offset)
-      when Curses::Key::DC
-        main_window.setpos(main_window.cury, main_window.curx - 1)
-      when 127 #backspace
-        if main_window.curx-1 >= 0
-        	buffer[main_window.cury-1][main_window.curx-1] = ''
-        	old_pos = [main_window.cury, main_window.curx]
-        	main_window.setpos(main_window.cury, 0)
-        	main_window.clrtoeol
-        	main_window.addstr(buffer[main_window.cury - 1])
-        	main_window.setpos(old_pos.first, old_pos.last- 1)
-        end
+        main_window.move(:left)
+      when RIGHT
+        main_window.move(:right)
+      when UP
+        main_window.move(:up)
+      when DOWN
+        main_window.move(:down)
+      when DC
+        #main_window.setpos(main_window.cury, current_x - 1)
+      when BACKSPACE
+        main_window.backspace
       else
-        buffer[lines] = "" if buffer[lines] == nil
-        buffer[main_window.cury + offset - 1].insert(main_window.curx, x.to_s)
-        old_pos = [main_window.cury, main_window.curx]
-        main_window.setpos(main_window.cury, 0)
-        writeln(main_window, (main_window.cury + offset - 1), buffer[main_window.cury + offset - 1], buffer)
-        main_window.setpos(old_pos.first, old_pos.last + 1)
+
       end
     end
     main_window.close
   end
+end
+
+class Window
+  attr_accessor :main_window, :buffer
+
+  def initialize
+    self.main_window = Curses::Window.new( 0, 0, 0, 0)
+    main_window.keypad(true)
+  end
+
+  def handle_return_key(key_input, buffer)
+    if key_input == 10
+      buffer.insert(main_window.cury, '')
+      old_pos = [main_window.cury, current_x]
+      redraw_screen(main_window, buffer)
+      main_window.setpos(old_pos.first + 1, old_pos.last)
+    end
+  end
+
+  def get_character
+    main_window.getch
+  end
+
+  def current_x
+    main_window.curx
+  end
+
+  def current_y
+    main_window.cury
+  end
+
+  def next_line
+    buffer[current_y]
+  end
+
+  def previous_line
+    buffer[current_y - 2]
+  end
+
+  def current_line
+    buffer[current_y - 1]
+  end
+
+  def color_on(value)
+    main_window.attron(Curses.color_pair(value))
+  end
+
+  def move(direction)
+    case direction
+    when :left
+      current_x < 2 ? main_window.setpos(current_y, 0) : main_window.setpos(current_y, current_x - 1)
+    when :right
+      main_window.setpos(current_y, current_x + 1)
+    when :up
+      redraw_screen(main_window, buffer, 0, offset)
+      current_y < 2 ? main_window.setpos(1, current_x) : main_window.setpos(current_y - 1, current_x)
+    when :down
+      redraw_screen(main_window, buffer, 0, offset)
+      current_y >= buffer.size ? main_window.setpos(buffer.size, current_x) : main_window.setpos(current_y + 1, current_x)
+    end
+  end
+
+  def backspace
+    if current_x-1 >= 0
+      buffer[main_window.cury-1][current_x-1] = ''
+      old_pos = [main_window.cury, current_x]
+      main_window.setpos(main_window.cury, 0)
+      main_window.clrtoeol
+      main_window.addstr(buffer[main_window.cury - 1])
+      main_window.setpos(old_pos.first, old_pos.last- 1)
+    end
+  end
+
+  def redraw_screen
+    index = 0
+    old_pos = [current_y, current_x]
+    (0...52).each do |num|
+      set_position(index + 1, 0)
+      main_window.clrtoeol
+      writeln()
+      index += 1
+    end
+    set_position(old_pos.first, old_pos.last)
+  end
+
+  def set_position(x, y)
+    previous_x = current_x
+    previous_y = current_y
+    main_window.setpos(x, y)
+  end
+
+  private
+
+  def writeln
+    main_window.refresh
+    main_window.addstr("x")
+  end
+
 end
 
 Editor.new.run
